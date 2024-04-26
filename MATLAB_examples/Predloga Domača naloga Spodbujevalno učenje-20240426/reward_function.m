877d1578e7999fc327083dde3f9e2703aafd2603
Gasper Jezernik <gj1182@student.uni-lj.si>
2024-04-26 11:21:09 +0200

    update
    matlab lahk ađlaufas na netu https://matlab.mathworks.com/

--- MATLAB_examples/Predloga Domača naloga Spodbujevalno učenje-20240426/reward_function.m
+++ MATLAB_examples/Predloga Domača naloga Spodbujevalno učenje-20240426/reward_function.m
@@ -1,28 +1,16 @@
-function reward = reward_function(next_state)
+function reward = reward_function(next_state, lake, n)
     % This function should take the next state and return the corresponding reward.
     % You need to fill in the logic based on your specific problem.
     
-    For example, if your reward is a function of the state, you might calculate the reward like this:
-    
+    %For example, if your reward is a function of the state, you might calculate the reward like this:
     reward = -1;  % default reward
-    if next_state == goal_state
-        reward = 100;  % reward for reaching the goal
-    elseif next_state == pit_state
-        reward = -100;  % pefunction reward = reward_function(next_state, lake, n)
-        % This function should take the next state and return the corresponding reward.
-        % You need to fill in the logic based on your specific problem.
-        
-        %For example, if your reward is a function of the state, you might calculate the reward like this:
-        reward = -1;  % default reward
-        % Convert state to row and column indices
-        [row, col] = ind2sub([n n], next_state);
-    
-        if lake(row, col) == n
-            reward = 500;  % reward for reaching the goal
-        end
-        if lake(row, col) == -n
-            reward = -800;  % reward for falling in the hole
-        end
-    endnalty for falling into a pit
+    % Convert state to row and column indices
+    [row, col] = ind2sub([n n], next_state);
+
+    if lake(row, col) == n
+        reward = 500;  % reward for reaching the goal
+    end
+    if lake(row, col) == -n
+        reward = -800;  % reward for falling in the hole
     end
 end
